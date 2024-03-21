d87f287498787e4c32a9b49a2629e2ffdb120822
Julian <jclatro@yahoo.com>
2024-03-21 10:05:49 -0300

    0.0.4-10a4da613125

--- app/components/CutEditor.tsx
+++ app/components/CutEditor.tsx
@@ -2,21 +2,21 @@ import React, { useState, useMemo, useCallback } from 'react';
 import { Descendant, createEditor, Transforms, Editor, Node, Range } from 'slate';
 import { ReactEditor, Slate, Editable, withReact, RenderElementProps, useSlateStatic } from 'slate-react';
 import { Popover } from '@headlessui/react';
-import { FountainTypes, FountainNode } from './types';
+import { FountainTypes, FountainNode, CustomText } from './types';
 // import { Toolbar } from './Toolbar';
 
 const assignSceneNumbers = () => {
     // Select all elements with the class 'scene_heading'
     const sceneElements = document.querySelectorAll('.scene_heading');
-  
+
     sceneElements.forEach((element, index) => {
-      // Scene numbers start from 1, so add 1 to the index
-      const sceneNumber = index + 1;
-  
-      // Set the 'data-prefix' attribute with the scene number
-      element.setAttribute('data-scene-number', sceneNumber.toString());
+        // Scene numbers start from 1, so add 1 to the index
+        const sceneNumber = index + 1;
+
+        // Set the 'data-prefix' attribute with the scene number
+        element.setAttribute('data-scene-number', sceneNumber.toString());
     });
-  };
+};
 
 
 const CutEditor: React.FC = () => {
@@ -41,9 +41,9 @@ const CutEditor: React.FC = () => {
         { type: 'parenthetical', children: [{ text: "(another beer raised)", type: 'parenthetical' }] },
         { type: 'dialogue', children: [{ text: "To retirement.", type: 'dialogue' }] },
     ]);
-    
-    
-    
+
+
+
     const renderElement = useCallback((props: RenderElementProps) => {
         const { element, children, attributes } = props;
         // console.log('attributes', attributes)
@@ -82,6 +82,12 @@ const CutEditor: React.FC = () => {
                 return <div className="page-break" {...attributes}>{children}</div>;
             case 'title_page':
                 return <div className="title_page" {...attributes}>{children}</div>;
+            case 'lyric':
+                return <div className="lyric" {...attributes}>{children}</div>;
+            case 'centered_text':
+                return <div className="centered_text" {...attributes}>{children}</div>;
+            case 'dual_dialogue':
+                return <div className="dual_dialogue" {...attributes}>{children}</div>;
             default:
                 return <p {...attributes}>{children}</p>;
         }
@@ -89,101 +95,37 @@ const CutEditor: React.FC = () => {
 
     const updateNodeIfNeeded = useCallback(() => {
         const { selection } = editor;
-
-        if (selection && Editor.isBlock(editor, editor.children[selection.focus.path[0]])) {
-            const [node, path] = Editor.node(editor, selection.focus.path);
-            console.log("Node.string(node)", Node.string(node))
-            console.log("path", path)
-            console.log('node', node)
-            // Check if the node's text length exceeds 3 characters
-            const nodeText = Node.string(node);
-            if (nodeText.length > 2) {
-                if (/^[A-Z]+$/.test(nodeText)) {
-                    Transforms.setNodes(editor, { type: 'character' }, { at: path });
-                }
-                if (nodeText.startsWith('!')) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'action' }, 
-                        { at: path }
-                    );
-                }
-                if (nodeText.startsWith('[[') && nodeText.endsWith(']]')) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'note' }, 
-                        { at: path }
-                    );
-                }
-                
-                if (nodeText.startsWith('EXT.') || nodeText.startsWith('INT.')) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'scene_heading' }, 
-                        { at: path }
-                    );
-                }
-
-                if (nodeText.startsWith('# ') || nodeText.startsWith('## ') || nodeText.startsWith('### ') || nodeText.startsWith('#### '))  {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'section' }, 
-                        { at: path }
-                    );
-                }
-
-                
-                if (nodeText.startsWith('~')) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'lyric' }, 
-                        { at: path }
-                    );
-                }
-
-                if (nodeText.startsWith('= ')) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'synopsis' }, 
-                        { at: path }
-                    );
-                }
-                
-                if (nodeText.startsWith('>') && nodeText.endsWith('<')) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'centered_text' }, 
-                        { at: path }
-                    );
-                }
-
-                if (nodeText.startsWith('(') && nodeText.endsWith(')')) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'parenthetical' }, 
-                        { at: path }
-                    );
-                }
-                const prefixes = ['Title:', 'Credits:', 'Author:', 'Source:', 'Draft Date:', 'Contact:'];
-                const matchesPrefix = prefixes.some(prefix => nodeText.startsWith(prefix));
-                if (matchesPrefix) {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'title_page' }, 
-                        { at: path }
-                    );
-                }
-
-                if (nodeText === '===') {
-                    Transforms.setNodes(
-                        editor,
-                        { type: 'page_break' }, 
-                        { at: path }
-                    );
-                }
-            }
+        if (!selection || !Editor.isBlock(editor, editor.children[selection.focus.path[0]])) {
+            return;
         }
+
+        const [node, path] = Editor.node(editor, selection.focus.path);
+        const nodeText = Node.string(node);
+        if (nodeText.length <= 2) return;
+
+        const updateType = (type: FountainTypes) => Transforms.setNodes(editor, { type }, { at: path });
+
+        const checkAndUpdateNode = () => {
+            if (/^[A-Z]+$/.test(nodeText)) return updateType('character');
+            if (nodeText.startsWith('!')) return updateType('action');
+            if (nodeText.startsWith('[[') && nodeText.endsWith(']]')) return updateType('note');
+            if (nodeText.startsWith('EXT.') || nodeText.startsWith('INT.')) return updateType('scene_heading');
+            if (nodeText.startsWith('~')) return updateType('lyric');
+            if (nodeText.startsWith('= ')) return updateType('synopsis');
+            if (nodeText.startsWith('>') && nodeText.endsWith('<')) return updateType('centered_text');
+            if (nodeText.startsWith('(') && nodeText.endsWith(')')) return updateType('parenthetical');
+            if (nodeText === '===') return updateType('page_break');
+
+            const prefixes = ['Title:', 'Credits:', 'Author:', 'Source:', 'Draft Date:', 'Contact:'];
+            if (prefixes.some(prefix => nodeText.startsWith(prefix))) return updateType('title_page');
+
+            // For checking section headers
+            if (/^#+ /.test(nodeText)) return updateType('section');
+        };
+
+        checkAndUpdateNode();
     }, [editor]);
+
 
     React.useEffect(() => {
         updateNodeIfNeeded();
@@ -200,14 +142,14 @@ const CutEditor: React.FC = () => {
         if (!editor.selection || !ReactEditor.isFocused(editor)) {
             return;
         }
-    
+
         const { selection } = editor;
         if (selection && Range.isCollapsed(selection)) {
             const domSelection = window.getSelection();
             if (domSelection && domSelection.rangeCount > 0) {
                 const range = domSelection.getRangeAt(0);
                 const rect = range.getBoundingClientRect();
-    
+
                 // Ensure the rect is valid
                 if (rect.bottom !== 0 || rect.left !== 0) {
                     setPopoverPos({
@@ -226,7 +168,7 @@ const CutEditor: React.FC = () => {
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
         console.log("event.key", event.key)
-    
+
         switch (event.key) {
             case 'Enter':
                 if (event.shiftKey) {
@@ -240,30 +182,34 @@ const CutEditor: React.FC = () => {
                     handleEnterPress();
                 }
                 break;
-    
+
             case 'Tab':
                 event.preventDefault();
                 Editor.insertText(editor, '    ');
                 break;
-    
+
             // Add other cases as needed
         }
     };
-    
+
     const handleEnterPress = () => {
         if (!editor.selection) return;
-    
+
         const currentPath = editor.selection.focus.path;
         let newNodeType: FountainTypes = 'action';
-    
+
         if (currentPath[0] > 0) {
-            // const previousPath = Path.previous(currentPath);
-            const previousPath = [...currentPath]
+            const previousPath = [...currentPath];
+
             try {
-                const previousNode = Node.get(editor, previousPath) as FountainNode;
+                const previousNode = Node.get(editor, previousPath) as CustomText;
                 console.log('Previous Node:', previousNode);
+
                 if (previousNode.type === 'character') {
-                    newNodeType = 'dialogue';
+                    // Check if the text ends with a caret '^' for dual dialogue
+                    const isDualDialogue = previousNode.text.endsWith('^');
+
+                    newNodeType = isDualDialogue ? 'dual_dialogue' : 'dialogue';
                 } else if (previousNode.type === 'dialogue') {
                     newNodeType = 'dialogue';
                 }
@@ -271,10 +217,11 @@ const CutEditor: React.FC = () => {
                 console.error('Error fetching previous node:', error);
             }
         }
-    
+
         insertNewNode(newNodeType);
     };
-    
+
+
     const insertNewNode = (newNodeType: FountainTypes) => {
         const newNode: FountainNode = { type: newNodeType, children: [{ text: '', type: newNodeType }] };
         Transforms.insertNodes(editor, newNode);
@@ -287,59 +234,6 @@ const CutEditor: React.FC = () => {
         setIsPopoverVisible(!isPopoverVisible); // Show the popover
         // You can add additional logic here if needed
     };
-
-    // const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    //     console.log("event.key", event.key)
-    //     if (event.key === 'Enter' && event.shiftKey) {
-    //         console.log("Shift + Enter was pressed");
-    //         // Handle the Shift+Enter key press here
-    //     }
-    //     if (event.key === 'Enter') {
-    //         event.preventDefault(); // Prevent default to handle the enter key manually
-
-    //         // Check if there is a current selection
-    //         if (editor.selection) {
-    //             const currentPath = editor.selection.focus.path;
-    //             let newNodeType: FountainTypes = 'action';
-    //             // First, check if there's a previous node and log it
-    //             if (currentPath[0] > 0) {
-    //                 // Manually construct the previous path
-    //                 const previousPath = [...currentPath];
-    //                 // Decrease the index to get the previous node
-
-    //                 try {
-                        
-    //                     const previousNode = Node.get(editor, previousPath) as FountainNode;
-    //                     console.log('Previous Node:', previousNode);
-    //                     if (previousNode.type === 'character') {
-    //                         newNodeType = 'dialogue';
-    //                     } else if (previousNode.type === 'dialogue') {
-    //                         newNodeType = 'dialogue';
-    //                     }
-    //                 } catch (error) {
-    //                     console.error('Error fetching previous node:', error);
-    //                 }
-    //             }
-
-    //             // Insert a line break at the current selection
-    //             // Editor.insertBreak(editor);
-
-    //             // Create a new node with type 'action'
-    //             console.log('newNodeType', newNodeType)
-    //             const newNode: FountainNode = { type: newNodeType, children: [{ text: '', type: newNodeType }] };
-
-    //             // Insert the new node at the selection
-    //             Transforms.insertNodes(editor, newNode);
-
-    //             // Optional: You might want to move the selection to the start of the new node
-    //             // Transforms.select(editor, Path.next(Path.parent(editor.selection.anchor.path)));
-    //         }
-    //         // setIsPopoverVisible(true);
-    //     } else if (event.key === 'Tab') {
-    //         event.preventDefault()
-    //         Editor.insertText(editor, '    ');
-    //     }
-    // };
 
     return (
         <Slate editor={editor} initialValue={value} onChange={handleEditorChange}>
@@ -370,21 +264,23 @@ const nodeTypes: { type: FountainTypes; label: string }[] = [
     { type: 'section', label: 'Section' },
     { type: 'synopsis', label: 'Synopsis' },
     { type: 'page_break', label: 'Page Break' },
-    { type: 'title_page', label: 'Title Page'}
+    { type: 'title_page', label: 'Title Page' }
 ];
 
 const Toolbar: React.FC = () => {
     const editor = useSlateStatic();
 
     const setNodeType = (type: FountainTypes) => {
-        const [match] = Editor.nodes(editor, {
-            match: n => Editor.isBlock(editor, n),
-            at: editor.selection ?? undefined,
-        });
-
-        if (match) {
-            Transforms.setNodes(editor, { type }, { at: editor.selection ?? undefined });
-        }
+        if (!editor.selection) return;
+    
+        // Get the current node and its path
+        const [node, path] = Editor.node(editor, editor.selection.focus.path);
+    
+        // Create a new object with the updated type
+        const newNode = { ...node, type: type };
+    
+        // Set the new node at the current path
+        Transforms.setNodes(editor, newNode, { at: path });
     };
 
     const isNodeTypeActive = (type: FountainTypes): boolean => {
--- app/components/types.d.ts
+++ app/components/types.d.ts
@@ -1,4 +1,4 @@
-export type FountainTypes = 'paragraph' | 'scene_heading' | 'character' | 'transition' | 'action' | 'parenthetical' | 'dialogue' | 'note' | 'section' | 'synopsis' | 'page_break' | 'title_page' | 'lyric' | 'centered_text'
+export type FountainTypes = 'paragraph' | 'scene_heading' | 'character' | 'transition' | 'action' | 'parenthetical' | 'dialogue' | 'note' | 'section' | 'synopsis' | 'page_break' | 'title_page' | 'lyric' | 'centered_text' | 'dual_dialogue'
 
 export type CustomElement = {
     type: FountainTypes;
@@ -6,7 +6,7 @@ export type CustomElement = {
 };
 
 export type CustomText = { text: string, type?: FountainTypes };
-export type FountainNode = { type: FountainTypes, children: { text: string, type: FountainTypes }[] };
+export type FountainNode = { type: FountainTypes, children: CustomText[] };
 
 declare module 'slate' {
     interface CustomTypes {
