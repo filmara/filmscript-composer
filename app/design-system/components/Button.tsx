f73502f3087def80e76ffc4bcd353deb955b2f55
Julian <jclatro@yahoo.com>
2024-03-25 10:01:11 -0300

    tw stories

--- app/design-system/components/Button.tsx
+++ app/design-system/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button: React.FC<ButtonProps> = ({
 }) => {
 
   return (
-    <div>
+    <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       {text}
     </div>
   );
--- sb-vite.config.ts
+++ sb-vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   process.env = { ...process.env, ...env }
-
   return {
-    // no Remix Vite plugin here
     plugins: [tsconfigPaths()],
+    resolve: {
+      alias: {
+        '~/styles': path.resolve(__dirname, './app/assets'),
+      },
+    },
   }
 })
