527c7a68697593971edaeb219ecea261871698bf
Julian <jclatro@yahoo.com>
2024-03-25 10:56:42 -0300

    fix colors

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,6 +8,19 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: '#F5F4F7'
+        },
+        {
+          name: 'dark',
+          value: '#1C181E'
+        }
+      ]
+    }
   },
 };
 
